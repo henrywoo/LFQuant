3d52baf9989d0aec0e1d7a974623411fcbff92b1
Henry Wu <wufuheng@gmail.com>
2024-07-04 11:28:39 -0700

    multi-gpu support

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+import os
+
+here = os.path.dirname(os.path.realpath(__file__))
+HAS_CUDA = os.system("nvidia-smi > /dev/null 2>&1") == 0
+
+VERSION = "0.0.1.dev2"
+DESCRIPTION = "LFQuant - Lookup-Free Quantization"
+
+packages = [
+    "lfquant",
+]
+
+
+def read_file(filename: str):
+    try:
+        lines = []
+        with open(filename) as file:
+            lines = file.readlines()
+            lines = [line.rstrip() for line in lines if not line.startswith("#")]
+        return lines
+    except:
+        return []
+
+
+setup(
+    name="lfquant",
+    version=VERSION,
+    author="Henry Wu",
+    description=DESCRIPTION,
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=read_file(f"{here}/requirements.txt"),
+    keywords=[
+        "lfquant",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    packages=packages,
+)
